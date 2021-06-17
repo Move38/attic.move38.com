b43f0f1418786d5a33848182a2f139ad55ff81c2
Jonathan Bobrow <jbobrow@gmail.com>
2021-06-17 09:55:09 -0400

    Add a desktop QR code generator

--- js/generator-desktop.js
+++ js/generator-desktop.js
@@ -8,15 +8,8 @@ var blinksterID = urlParams.get('ref');
 var discount = urlParams.get('discount');
 //console.log(blinksterID);
 
-// Load a desktop version if visited not on a mobile device
-if (screen.width >= 500) {
-	   document.location = "../desktop-generator.html?discount="+discount+"&ref="+blinksterID;
-	   /* Sends the user to a desktop formatted site ... */
-	}
-	
-
 var _statcounter = _statcounter || []; 
-_statcounter.push({"tags": {"blinksterID": blinksterID, "discount": discount, "page": "generator"}}); 
+_statcounter.push({"tags": {"blinksterID": blinksterID, "discount": discount, "page": "generator-desktop"}}); 
 
 /*
 Generate a QR Code with the following Style
@@ -32,7 +25,7 @@ if(discount === null) {
 }
 
 var qrCodeURL = "https://vip.move38.com/?discount=" + discount + "&ref=" + blinksterID;
-var qrSize = window.innerWidth * 0.6 - 20;
+var qrSize = 400;
 
 const qrCode = new QRCodeStyling({
 	width: qrSize,
@@ -58,6 +51,7 @@ qrCode.append(document.getElementById("canvas"));
 // update the padding after
 document.getElementById("canvas").style.padding = "10px";
 document.getElementById("canvas").style.width = qrSize;
+document.getElementById("canvas").style.margin = "auto";
 
 // update the secret link at the bottom
 if(blinksterID === null) {
